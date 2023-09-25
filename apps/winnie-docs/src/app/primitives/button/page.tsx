c5a532fb804f26b5b0f6bc92a8b24e4afef09f79
adamaho <aho.adam01@gmail.com>
2023-09-24 21:56:46 -0400

    removing radius from button

--- apps/winnie-docs/src/app/primitives/button/page.tsx
+++ apps/winnie-docs/src/app/primitives/button/page.tsx
@@ -2,11 +2,9 @@ import { Button } from "winnie-react/button";
 
 export default function Page() {
 	return (
-		<div className="row">
+		<div w-display="flex" w-align-items="center" w-gap="3">
 			<Button radius="large">Button</Button>
-			<Button variant="soft" radius="round">
-				Button
-			</Button>
+			<Button variant="soft">Button</Button>
 			<Button variant="ghost">Button</Button>
 		</div>
 	);
