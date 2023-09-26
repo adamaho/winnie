0aa5b110230873b472b1972712f9c0532f1c467d
adamaho <aho.adam01@gmail.com>
2023-09-26 11:12:02 -0400

    adding some more stuff

--- apps/winnie-docs/src/app/primitives/button/page.tsx
+++ apps/winnie-docs/src/app/primitives/button/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "winnie-react/button";
 export default function Page() {
 	return (
 		<div w-display="flex" w-align-items="center" w-gap="3">
-			<Button radius="large">Button</Button>
+			<Button>Button</Button>
 			<Button variant="soft">Button</Button>
 			<Button variant="ghost">Button</Button>
 		</div>
